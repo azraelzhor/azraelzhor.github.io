abf59a4a6a4dae3cdf5f9701245123b2c4e36691
azraelzhor <conduongtoluat0412@gmail.com>
2020-05-24 11:35:27 +0700

    Deploy

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -12,5 +12,20 @@ window.MathJax = {
   },
   loader: {
     load: ['[tex]/noerrors']
-  }
+  },
+  svg: {
+    displayAlign: 'center',        // default for indentalign when set to 'auto'
+    displayIndent: '0',            // default for indentshift when set to 'auto'
+    fontCache: 'local',            // or 'global' or 'none'
+  },
+  // options: {
+  //   ignoreHtmlClass: "tex2jax_ignore"
+  // }
 };
+
+// (function () {
+//   var script = document.createElement('script');
+//   script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+//   script.async = true;
+//   document.head.appendChild(script);
+// })();
